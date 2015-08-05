dd294b76c6e07305450f178c9343f9200f3a93a3
CSSI 2015 <CSSI@CSSIs-MacBook-Air.local>
2015-08-05 17:16:42 -0500

    fixed js

--- finding-android1/scripts/javascript.js
+++ finding-android1/scripts/javascript.js
@@ -1,11 +1,40 @@
 $(document).ready(function(){
-   $(".next_level_button").hide()
+   $("#next_level_button_1").hide()
     $("#android_level1").click(function(){
       $("#android_level1").height(200).width(200)
-     animateAndroid();
-     $(".next_level_button").fadeIn()
+     animateAndroid_1();
+     $("#next_level_button_1").fadeIn()
    });
+  });
+
+$(document).ready(function(){
+  $("#next_level_button_2").hide()
+  $("#android_level2").click(function(){
+    $("#android_level2").height(200).width(200)
+    animateAndroid_2();
+    $("#next_level_button_2").fadeIn()
 });
+});
+
+$(document).ready(function(){
+  $("#next_level_button_3").hide()
+  $("#android_level3").click(function(){
+    $("#android_level3").height(200).width(200)
+    animateAndroid_3();
+    $("#next_level_button_3").fadeIn()
+});
+});
+
+$(document).ready(function(){
+  $("#next_level_button_4").hide()
+  $("#android_level4").click(function(){
+    $("#android_level4").height(200).width(200)
+    animateAndroid_2();
+    $("#next_level_button_4").fadeIn()
+});
+});
+
+
 
 function makeNewPosition(){
     // Get viewport dimensions (remove the dimension of the div)
@@ -17,9 +46,38 @@ function makeNewPosition(){
 
     return [nh,nw];
 }
-function animateAndroid(){
+
+function animateAndroid_1(){
     var newq = makeNewPosition();
     $("#android_level1").animate({ top: newq[0], left: newq[1] }, function(){
-      animateAndroid();
+      animateAndroid_1();
+    });
+};
+
+function animateAndroid_2(){
+    var newq = makeNewPosition();
+    $("#android_level2").animate({ top: newq[0], left: newq[1] }, function(){
+      animateAndroid_2();
+    });
+};
+
+function animateAndroid_3(){
+    var newq = makeNewPosition();
+    $("#android_level1").animate({ top: newq[0], left: newq[1] }, function(){
+      animateAndroid_3();
+    });
+};
+
+function animateAndroid_4(){
+    var newq = makeNewPosition();
+    $("#android_level1").animate({ top: newq[0], left: newq[1] }, function(){
+      animateAndroid_4();
+    });
+};
+
+function animateAndroid_5(){
+    var newq = makeNewPosition();
+    $("#android_level1").animate({ top: newq[0], left: newq[1] }, function(){
+      animateAndroid_5();
     });
 };
